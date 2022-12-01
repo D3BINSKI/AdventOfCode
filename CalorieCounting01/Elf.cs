97bac0444be54e74d8c996f04584c1981de9cf80
Dominik Debinski <debinskid16@gmail.com>
2022-12-01 22:20:41 +0100

    Day1 tasks done

---
+++ CalorieCounting01/Elf.cs
@@ -0,0 +1,23 @@
+﻿namespace CalorieCounting01
+{
+    public class Elf
+    {
+        private static int _id = 0;
+        private int id;
+        private List<Provision> _provisions;
+        
+        public List<Provision> Provisions => _provisions;
+
+        public Elf(List<Provision> provisions)
+        {
+            id = _id++;
+            _provisions = provisions;
+        }
+    
+        public void AddProvision(Provision provision)
+        {
+            _provisions.Add(provision);
+        }
+    }
+}
+
---
+++ CalorieCounting01/Provision.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CalorieCounting01
+{
+    public class Provision
+    {
+        public int Calories { get; }
+
+        public Provision(int calories)
+        {
+            Calories = calories;
+        }
+    }
+}
+
